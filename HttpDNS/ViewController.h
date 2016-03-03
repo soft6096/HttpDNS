ea8cdc6f3bba44e05b722b1c2a877253511da747
jianli63 <jianli63@creditease.cn>
2016-03-03 15:04:43 +0800

    Initial Commit

---
+++ HttpDNS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpDNS
+//
+//  Created by Weblogic on 16/3/3.
+//  Copyright © 2016年 Weblogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
